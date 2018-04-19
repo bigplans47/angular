4abdaf5e79895ef8ab7ee32991499cac519b9423
Dan Kiss <flowfast47@gmail.com>
2018-04-19 11:23:56 -0700

    add query using a join table to class List

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,18 @@
 class List < ActiveRecord::Base
   has_many :tasks
   validates :name, :presence => true
+  scope :most_tasks, -> {(
+    select("lists.id, lists.name, count(tasks.id) as tasks_count")
+    .joins(:tasks)
+    .group("lists.id")
+    .order("tasks_count DESC")
+    .limit(10)
+  )}
+  scope :most_tasks_top_2, -> {(
+    select("lists.id, lists.name, count(tasks.id) as tasks_count")
+    .joins(:tasks)
+    .group("lists.id")
+    .order("tasks_count DESC")
+    .limit(2)
+    )}
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,13 @@ class Task < ActiveRecord::Base
   def self.not_done
     where(done: false)
   end
-  scope(:is_done, -> {
+  scope(:is_done, -> do
     where({:done => true})
-  })
+  end)
+  scope :today, -> {
+    where("created_at >=?", Time.now.beginning_of_day)
+  }
+  scope :five_most_recent, -> {
+    order(:created_at => :desc).limit(5)
+  }
 end
