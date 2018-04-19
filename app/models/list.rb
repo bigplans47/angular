7dc6d027035a3e70ce6fedd08ce6d97f1968accf
Dan Kiss <flowfast47@gmail.com>
2018-04-19 12:00:51 -0700

    add search functionality to the to_do program in class List, can be accessed in rails console, has both specific and less specific search functionality

--- app/models/list.rb
+++ app/models/list.rb
@@ -8,11 +8,17 @@ class List < ActiveRecord::Base
     .order("tasks_count DESC")
     .limit(10)
   )}
-  scope :most_tasks_top_2, -> {(
+  scope :most_tasks_top_2, -> {
     select("lists.id, lists.name, count(tasks.id) as tasks_count")
     .joins(:tasks)
     .group("lists.id")
     .order("tasks_count DESC")
     .limit(2)
-    )}
+  }
+  scope :find_name, -> (name_parameter) {
+    where(:name => name_parameter)
+  }
+  scope :find_name_not_exact, -> (name_parameter) {
+    where("name like?", "%#{name_parameter}%")
+  }
 end
